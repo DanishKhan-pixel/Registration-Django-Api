3bfa87d9f87f1a592ac52a766396ff846dc20750
MUHAMMAD DANISH <96936557+DanishKhan-pixel@users.noreply.github.com>
2026-08-09 00:03:46 +0500

    feat: add LOGIN_TYPE_CHOICES to authentication/choices.py for user login options

--- authentication/choices.py
+++ authentication/choices.py
@@ -9,11 +9,19 @@ TYPE_CHOICES = (
     ('token_expire', 'Token Expire'),
 )
 
+LOGIN_TYPE_CHOICES = (
+    ('email', 'Email'),
+    ('phone', 'Phone'),
+    ('google', 'Google'),
+    ('facebook', 'Facebook'),
+    ('apple', 'Apple'),
+)
 TOKEN_TYPE_CHOICES = (
     ('email_verification', 'Email Verification'),
     ('subscription_token', 'Subscription Token'),
     ('forgot_password', 'Password Forgotten'),
 )
+
 
 MEDIA_TYPES_CHOICES = (
     ('image', 'Image'),
